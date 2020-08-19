dfdcd5dc22c01e26f777139585556b981b6422be
dell <vagner@envixo.com>
2020-08-18 21:25:41 -0300

    inicia criação de posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root "posts#index"
   #sessions para logar
   #registrations para criar uma conta
   #passwords para resetar a senha
 
-  devise_for :users, only: %[sessions registrations passwords]
+  devise_for :users, only: %i[sessions registrations passwords]
 
   resources :posts, only: %i[index show new create]
+
+  resources :users, only: %i[show], param: :username # substitui o id por username
 end
